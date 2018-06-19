28010d3c008afcd51a3b204dbcb697c95f2ca530
Matouche <Matouche@users.noreply.github.com>
2018-06-19 23:15:28 +0200

    First Pelican Commit

---
+++ content/pages/temp.md
@@ -0,0 +1,15 @@
+Title: Mathis Dubrul - Portfolio
+URL:
+save_as: index.html
+Template: temp
+
+Étudiant en 2e année en Animation 2D et Illustration Numérique à la Haute École Albert Jacquard
+
+Mon portfolio est encore en construction, veuillez m'excuser pour le désagrément.
+
+**Fin des travaux prévue pour le 21 juin au plus tard, n'hésitez pas à revenir.**
+
+En attendant, vous pouvez dès à présent regarder mon Travail de Fin d'Année en Animation 2D, [LITTLE AGENT](https://ncloud.zaclys.com/index.php/s/6n4Bg8mKRm6AgGA) (mot de passe : *juin2018*).
+
+À bientôt au Career's day,  
+Mathis Dubrul
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Mathis Dubrul'
+SITENAME = 'Mathis Dubrul - Animateur/Illustrateur 2D'
+SITEURL = 'https://matouche.github.io'
+
+THEME = 'mportfolio'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('Vimeo', 'https://vimeo.com/user12625616'),
+          ('ArtStation', 'matouche.artstation.com'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Useful settings :
+DEFAULT_DATE = 'fs'
+INDEX_SAVE_AS = 'posts.html'
+STATIC_PATHS = ['images', 'extra']
+PAGE_EXCLUDES = ['extra']
+ARTICLE_EXCLUDES = ['extra']
+EXTRA_PATH_METADATA = {'extra/README.md' : {'path': 'README.md'},
+                       'extra/specials/' : {'path': 'specials/'},}
